1d0296ee29a459ace628ac2dd3cb3e88f63c308e
Oscar Polanco <itt.oscarpolanco@gmail.com>
2018-01-12 14:04:38 -0400

    base app with a requirement file

--- paradise_papers_search/paradise_papers_search/urls.py
+++ paradise_papers_search/paradise_papers_search/urls.py
@@ -1,4 +1,4 @@
-"""paradise_papers_django URL Configuration
+"""paradise_papers_search URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -13,10 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include,url
+from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^search/', include('search.urls', namespace='search')),
 ]
--- paradise_papers_search/paradise_papers_search/wsgi.py
+++ paradise_papers_search/paradise_papers_search/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for paradise_papers_django project.
+WSGI config for paradise_papers_search project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "paradise_papers_django.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "paradise_papers_search.settings")
 
 application = get_wsgi_application()
