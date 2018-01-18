8562b0e05563c94aa63b484fb6ad44c171cfae7a
Maria Mera <mmera@intellisys.com.do>
2018-01-18 10:05:34 -0400

    Add the relationship connections for the address node

--- paradise_papers_search/fetch_api/models/Address.py
+++ paradise_papers_search/fetch_api/models/Address.py
@@ -1,15 +1,17 @@
 from neomodel import *
 from neomodel import db
 from django_neomodel import DjangoNode
+from . import helpers
 
 class Address(DjangoNode):
-    sourceID      = StringProperty()
-    country_codes = StringProperty()
-    valid_until   = StringProperty()
-    address       = StringProperty()
-    countries     = StringProperty()
-    node_id       = StringProperty()
-
+    sourceID       = StringProperty()
+    country_codes  = StringProperty()
+    valid_until    = StringProperty()
+    address        = StringProperty()
+    countries      = StringProperty()
+    node_id        = StringProperty()
+    officers       = RelationshipFrom('.Officer.Officer', 'REGISTERED_ADDRESS')
+    intermediaries = RelationshipFrom('.Intermediary.Intermediary', 'REGISTERED_ADDRESS')
     @property
     def serialize(self):
         return {
@@ -22,3 +24,15 @@ class Address(DjangoNode):
                 'node_id': self.node_id,
             },
         }
+    @property
+    def serialize_connections(self):
+        return [
+            {
+                'nodes_type': 'Officer',
+                'nodes_related': helpers.serialize_relationships(self.officers.all(), 'REGISTERED_ADDRESS'),
+            },
+            {
+                'nodes_type': 'Intermediary',
+                'nodes_related': helpers.serialize_relationships(self.intermediaries.all(), 'REGISTERED_ADDRESS'),
+            },
+    ]
