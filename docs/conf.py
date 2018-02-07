ed3ee206c76bf373aca84fcc25cce9d9b05f434a
Maria Mera <mmera@intellisys.com.do>
2018-02-07 14:40:37 -0400

    Add the navigation bar with the topics

--- docs/conf.py
+++ docs/conf.py
@@ -91,7 +91,7 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = { 'fixed_sidebar': True,}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -107,6 +107,7 @@ html_sidebars = {
     '**': [
         'relations.html',  # needs 'show_related': True theme option to display
         'searchbox.html',
+        'navigation.html',
     ]
 }
 
